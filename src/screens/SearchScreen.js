33520abe2dea96f489c79680c601e1df648a69a2
kescript <nnamanikester@gmail.com>
2020-05-06 21:28:45 +0100

    Made requests to the yelp api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,21 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
 
   return (
     <View>
       <SearchBar
-        onTermSubmit={() => console.log("submitted")}
+        onTermSubmit={() => searchApi(term)}
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermChange={setTerm}
       />
-      <Text>Search Scrren</Text>
-      <Text>{term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
